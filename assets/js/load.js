1cbe3a5becd54decaf79621a98ba6e866780997d
sqw1sh <98525753+sqw1sh@users.noreply.github.com>
2023-08-31 14:45:54 +0300

    Update load.js

--- assets/js/load.js
+++ assets/js/load.js
@@ -2,7 +2,7 @@ import {getCookie, setCookie} from './cookie.js';
 
 function loadGame() {
   $.getJSON('http://gclicker/game/characters/DEFAULT.json', function(data) {
-    
+
       for(let i=0;i<data.length;i++) {
         localStorage.setItem("character_" + data[i].id, JSON.stringify(data[i]));
       };
@@ -15,4 +15,14 @@ function loadGame() {
 
       localStorage.setItem("gClicker", JSON.stringify(gClicker));
   });
+}
+
+loadBtn.onclick = loadGame;
+
+if(localStorage.getItem("gClicker")) {
+  loadBtn.style.display = 'none';
+  document.querySelector('.load').innerHTML = `<a href="game/index.html"><button type="button" class="btn btn-outline-info" id="play">Продолжить игру</button></a>`;
+} else {
+  loadBtn.style.display = '';
+  play.style.display = 'none';
 }
