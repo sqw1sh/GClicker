b1aae6a709a5e0017989d8ad2d45ed4823098b93
sqw1sh <98525753+sqw1sh@users.noreply.github.com>
2023-08-31 13:36:36 +0300

    Update path.php

--- path.php
+++ path.php
@@ -1,7 +1,7 @@
 <?php
 
 const SITE_ROOT = __DIR__;
-const BASE_URL = "http://gclicker/";
+const BASE_URL = "https://sqw1sh.github.io/GClicker/";
 define("ROOT_PATH", realpath(dirname(__FILE__)));
 
  ?>
