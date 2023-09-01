c5ee8fa5a205a8e89e0a654bfe6e587bcc15950c
sqw1sh <98525753+sqw1sh@users.noreply.github.com>
2023-09-01 15:23:16 +0300

    Update load.js

--- assets/js/load.js
+++ assets/js/load.js
@@ -25,3 +25,31 @@ if(localStorage.getItem("gClicker")) {
 } else {
   loadBtn.style.display = '';
 }
+
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+if(getOS() == 'iOS' || getOS() == 'Android') {
+  loadBtn.display = "none";
+  document.querySelector(".device").display = "block";
+}
